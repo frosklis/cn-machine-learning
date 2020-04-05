3ad880197bf62a0c25beeee0d5b02f93b3d6f28e
Claudio Noguera <claudio.noguera@gmail.com>
2020-04-05 20:40:54 +0200

    lint

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,12 @@
 import os
 
-import pytest
 import pandas as pd
+import pytest
+
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 @pytest.fixture
 def titanic():
     train = os.path.join(THIS_DIR, 'test-files/titanic/train.csv')
     return pd.read_csv(train)
-    
