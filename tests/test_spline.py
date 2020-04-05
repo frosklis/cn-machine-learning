3b4921e671c637aba8a5d7594a4e6865b231d116
Claudio Noguera <claudio.noguera@gmail.com>
2020-04-05 19:05:31 +0200

    more thorough testing

--- tests/test_spline.py
+++ tests/test_spline.py
@@ -1,59 +1,33 @@
 import numpy as np
+import pytest
 
 from cnml.spline import Spline
 
 
-def test_sin():
-    X = np.linspace(0, 6.28, 1000)
+@pytest.mark.parametrize(
+    "offset", [0, -1000, -500, -100, -50, -5,
+               1000, 500, 100, 50, 5, ]
+)
+@pytest.mark.parametrize('allowed_error', [0.1])
+def test_numerical_stability(offset, allowed_error):
+    X = np.linspace(0, 6.28, 1000) + offset
     y = np.sin(X)
     model = Spline().fit(X, y)
     y_hat = model.predict(X)
 
     error = np.abs(y - y_hat)
-    assert error.max() < .1
+    assert error.max() < allowed_error
 
 
-def test_sin_stable():
-    X = np.linspace(0+10000, 6.28+10000, 1000)
+@pytest.mark.parametrize('degree_first', [0, 1, 2, 3])
+@pytest.mark.parametrize('degree_last', [0, 1, 2, 3])
+@pytest.mark.parametrize('num_knots', [0, 1, 2, 3, 4])
+def test_parameters(degree_first, degree_last, num_knots):
+    X = np.linspace(0, 6.28, 100)
     y = np.sin(X)
-    model = Spline().fit(X, y)
-    y_hat = model.predict(X)
-
-    error = np.abs(y - y_hat)
-    assert error.max() < .1
-
-
-
-def test_sin_0():
-    X = np.linspace(0, 6.28, 1000)
-    y = np.sin(X)
-    model = Spline(degrees=(3, 1), num_knots=4).fit(X, y)
-    y_hat = model.predict(X)
-
-    error = np.abs(y - y_hat)
-    assert error.max() < .1
-
-def test_grade_2():
-    X = np.linspace(0, 6.28, 1000)
-    y = np.sin(X)
-    model = Spline(degrees=(2, 2), num_knots=5).fit(X, y)
-    y_hat = model.predict(X)
-
-    error = np.abs(y - y_hat)
-    assert error.max() < .1
-
-def test_no_knots():
-    X = np.linspace(0, 6.28, 1000)
-    y = np.sin(X)
-    model = Spline(num_knots=0).fit(X, y)
-    y_hat = model.predict(X)
-    assert np.isnan(y_hat).sum() == 0, "there are null values in the output"
-    assert len(y_hat) == len(y), "correct number of data points"
-
-def test_no_average():
-    X = np.linspace(0, 6.28, 1000)
-    y = np.sin(X)
-    model = Spline(num_knots=0, degrees=(0,0)).fit(X, y)
+    model = Spline(degrees=(degree_first, degree_last),
+                   num_knots=num_knots).fit(
+        X, y)
     y_hat = model.predict(X)
     assert np.isnan(y_hat).sum() == 0, "there are null values in the output"
     assert len(y_hat) == len(y), "correct number of data points"
